3a69230fba4e063aa509d551cce690b011a8b60a
Umahoro Ntagana <goloobab@gmail.com>
2022-12-05 14:55:34 +0000

    added questions in the questions js file

---
+++ assets/js/questions.js
@@ -0,0 +1,25 @@
+// Questions (Array of objects) for the quiz
+
+const questions = [
+    {
+        title: 'How many title tags are in HTML ?',
+        answerChoices: ["7 tags","5 tags","6 tags","4 tags"],
+        correctAnswer: "6 tags"
+    },
+    {
+        title: 'What does HTML stand for?',
+        answerChoices: ["Hyperlinks and Text Markup Language", "Hyper Text Markup Language", "Home Tool Markup Language", "High Texture Mark Link"],
+        correctAnswer: "Hyper Text Markup Language"
+    },
+    {
+        title: 'Choose the correct HTML element for the largest heading:',
+        answerChoices: ["<heading>", "<h6>", "<head>", "<h1>"],
+        correctAnswer: "<h1>"
+    },
+    {
+        title: 'Which of these elements are all <table> elements?',
+        answerChoices: ["<thead> <body> <tr>", "<table> <tr> <td>", "<table> <head> <tfoot>", "<table> <tr> <tt>"],
+        correctAnswer: "<table> <tr> <td>"
+    }
+]
+
